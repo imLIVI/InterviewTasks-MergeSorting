1128aa0806eab049713ee8a8632f841bc572748d
imLIVI <lesly120898@gmail.com>
2023-01-18 18:24:12 +0300

    Rename class

--- src/main/java/exceptions/InvalidFileNameException.java
+++ src/main/java/exceptions/InvalidFileNameException.java
@@ -1,7 +1,7 @@
 package exceptions;
 
-public class InvalidFileName extends RuntimeException {
-    public InvalidFileName(String fileName) {
+public class InvalidFileNameException extends RuntimeException {
+    public InvalidFileNameException(String fileName) {
         System.out.println("[ERROR] Invalid name of file: " + fileName +
                 ". Check that the file name matches the template: *.txt");
     }
